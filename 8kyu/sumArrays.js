2d40c815a87a2959f0800d4657614655ef76792d
michelle armstrong <linmicarm@gmail.com>
2026-04-20 06:53:17 -0400

    Sum an array

--- 8kyu/sumArrays.js
+++ 8kyu/sumArrays.js
@@ -1,15 +1,5 @@
 /*
 Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative. If the array is empty, return 0.
-
-Examples
-Input: [1, 5.2, 4, 0, -1]
-Output: 9.2
-
-Input: [-2.398]
-Output: -2.398
-
-Input: []
-Output: 0
 */
 
 //My solution://
