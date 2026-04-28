44b3fb505d5bbb207f1e0e0066f668828d5f21b8
michelle armstrong <linmicarm@gmail.com>
2026-04-28 05:35:46 -0400

    Add files via upload

---
+++ 8kyu/binToDecimal.js
@@ -0,0 +1,10 @@
+/*
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/
+
+//My Solution://
+
+//Declcare a fucntion
+function binToDec(bin) {
+  return parseInt(bin, 2);
+}
